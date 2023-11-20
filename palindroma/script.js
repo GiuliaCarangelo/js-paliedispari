621bf0673d472149f7fa2b807e56f2686fa5b872
GiuliaCarangelo <tnhkagura@gmail.com>
2023-11-20 20:58:57 +0100

    add html, script.js

---
+++ palindroma/script.js
@@ -0,0 +1,14 @@
+// Palindroma:
+// E' vietato usare split(), reverse() e simili.
+// Consigli del giorno
+// Scriviamo sempre in italiano i passaggi che vogliamo fare
+// Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+
+
+// Chiedere all’utente di inserire una parola
+let userChoice = prompt("Type a word: ")
+
+// Creare una funzione per capire se la parola inserita è palindroma
+function isWordPalindrome(word){
+    if(word = )
+}
